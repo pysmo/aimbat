fdbd2be327de3fe5cf2714900bfe56161ef11ffd
Xiaoting Lou <xlou@u.northwestern.edu>
2012-09-27 19:34:01 -0500

    modify in scripts/egplot.py and scripts/egsac.py: from pysmo.sac.sacio import sacfile

--- scripts/egplot.py
+++ scripts/egplot.py
@@ -11,38 +11,31 @@ import matplotlib.transforms as transforms
 from pysmo.aimbat.sacpickle import loadData
 from pysmo.aimbat.plotphase import getDataOpts, PPConfig, sacp1, sacp2, sacprs
 
-
-# figure
+# figure axes
 fig = figure(figsize=(9,12))
-
 rectp2 = [.09, .050, .8, .15]
 rectp1 = [.09, .245, .8, .33]
 rectp0 = [.09, .620, .8, .36]
-
 axp2 = fig.add_axes(rectp2)
 axp1 = fig.add_axes(rectp1)
 axp0 = fig.add_axes(rectp0)
 
-# read data and then plot
+# read data and plot
 gsac, opts = getDataOpts()
-
 # prs
 opts.ynorm = .95
 saclist = gsac.saclist
 prs = sacprs(saclist, opts, axp0)
-
 # p1
 opts.ynorm = 1.7 
 p1 = sacp1(saclist, opts, axp1)
-
 # p2
 opts.reltime = 0
 p2 = sacp2(saclist, opts, axp2)
-
+# set x limits
 axp0.set_xlim(625, 762)
 axp1.set_xlim(625, 762)
 axp2.set_xlim(-45, 65)
-
 # numbering
 axs = [axp0, axp1, axp2]
 labs = 'ABC'
--- scripts/egsac.py
+++ scripts/egsac.py
@@ -6,7 +6,7 @@ Xiaoting Lou (xlou@u.northwestern.edu)
 03/07/2012
 """
 
-from pysmo.sacio import sacfile
+from pysmo.sac.sacio import sacfile
 from numpy import linspace, array
 from scipy import signal
 import matplotlib.pyplot as plt
@@ -29,14 +29,12 @@ y2 = signal.resample(y, nptsnew)
 fig = plt.figure(figsize=(12,4))
 ax = fig.add_subplot(111)
 trans = transforms.blended_transform_factory(ax.transAxes, ax.transAxes)
-
 plt.plot(x,  y, 'b-',  label='Delta = {0:.3f} s'.format(delta))
 plt.plot(x2, y2,'r--', label='Delta = {0:.3f} s'.format(deltanew))
 plt.xlabel('Time [s]')
 plt.legend(loc=2)
 plt.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
 ax.text(0.98, 0.9, ifilename, transform=trans, va='center', ha='right')
-
 plt.subplots_adjust(left=0.05,right=0.98,bottom=0.13,top=0.9)
 plt.xlim(600,900)
 plt.ylim(-1.2e-5,1.8e-5)
