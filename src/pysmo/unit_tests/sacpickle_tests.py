6c6f938574f3808dd71b8a9403595716e74bbe92
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-13 18:30:41 -0500

    tested hour

--- src/pysmo/unit_tests/sacpickle_tests.py
+++ src/pysmo/unit_tests/sacpickle_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import sys, os, os.path
-from pysmo.aimbat.sacpickle import readPickle, zipFile, fileZipMode, pkl2sac, sac2pkl, SacDataHdrs
+from pysmo.aimbat.sacpickle import readPickle, zipFile, fileZipMode, pkl2sac, sac2pkl, SacDataHdrs, SacGroup
 import numpy as np
 
 import obspy as obs
@@ -79,10 +79,46 @@ class sacpickleModel(unittest.TestCase):
     def test_SacDataHdrs_init(self):
         instance = SacDataHdrs('sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.113A.__.BHZ', 0.025)
         trace = obs.read('sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.113A.__.BHZ')
-        
+
         self.assertEqual(np.floor(instance.delta*1000),25)
         self.assertTrue(set(instance.data).issubset(trace[0].data))
         self.assertEqual(trace[0].stats.npts, instance.npts)
+
+    def test_SacGroup_init(self):
+        ifiles = ['sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.113A.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.319A.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.U15A.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.W13A.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.X16A.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AR.X18A.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.BZN.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.CPE.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.CRY.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.FRD.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.HWB.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.KNW.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.LVA2.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.MONP2.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.PFO.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.RDM.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.SCI2.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.SMER.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.SND.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.SOL.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.TRO.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/AZ.WMC.__.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/BK.CMB.00.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/BK.HUMO.00.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/BK.MCCM.00.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/BK.SAO.00.BHZ', 
+            'sac2pkl_files/Event_2011.09.15.19.31.04.080/BK.WDC.00.BHZ']
+        instance = SacGroup(ifiles, 0.025)
+
+        self.assertEqual(len(instance.saclist), len(ifiles))
+        self.assertEqual(instance.event[0], 2011) #year
+        self.assertEqual(instance.event[1], 9) #month
+        self.assertEqual(instance.event[2], 15) #day
+        self.assertEqual(instance.event[3], 19) #hour of day
 
 
 
