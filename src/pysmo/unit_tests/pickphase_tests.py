6d6d227636002d809ed48934b36261f876e02556
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-16 18:54:41 -0500

    not equal headers

--- src/pysmo/unit_tests/pickphase_tests.py
+++ src/pysmo/unit_tests/pickphase_tests.py
@@ -56,14 +56,7 @@ class pickphaseView(unittest.TestCase):
     	setattr(opts1,'labelqual',True)
     	ppm1 = PickPhaseMenu(gsac1, opts1, axs1)
 
-    	# randomly get stuff
-    	rand_order = random.randint(1, 4) 
-    	rand_lowFreq = random.uniform(0.1,0.2)
-    	rand_highFreq = random.uniform(0.8,1.2)
-
-    	opts1.filterParameters['lowFreq'] = rand_lowFreq
-    	opts1.filterParameters['highFreq'] = rand_highFreq
-    	opts1.filterParameters['order'] = rand_order
+    	originalSaclist = gsac1.saclist
 
     	# click the save button
     	click_save_event = matplotlib.backend_bases.MouseEvent('button_press_event', ppm1.axpp.figure.canvas, 63, 481)
@@ -73,9 +66,14 @@ class pickphaseView(unittest.TestCase):
     	click_params_event = matplotlib.backend_bases.MouseEvent('button_press_event', ppm1.axpp.figure.canvas, 415, 54)
     	ppm1.save_headers_filterParams(click_params_event)
 
-    	# reload to make sure parameters exist now
+    	# reload to make sure data has been overwritten
     	sys.argv[1:] = ['temp-test-bhz.pkl']
     	gsac2, opts2 = getDataOpts()
+
+    	for i in xrange(len(gsac2.saclist)):
+    		self.assertNotEqual(gsac1.saclist[i], gsac2.saclist[i])
+
+
 
     	
 
