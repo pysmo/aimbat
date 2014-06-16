03decb003c10a07e08e4ee17d4ac3f9b321cf27b
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-16 17:23:34 -0500

    clicking the save button

--- src/pysmo/unit_tests/pickphase_tests.py
+++ src/pysmo/unit_tests/pickphase_tests.py
@@ -1,6 +1,7 @@
 import unittest
 import sys, os, matplotlib
-from pickphase import PickPhaseMenu
+from pysmo.aimbat.qualctrl import getOptions, getDataOpts, sortSeis, getAxes, PickPhaseMenuMore
+from pysmo.aimbat.pickphase import PickPhaseMenu
 
 
 # ############################################################################### #
@@ -10,8 +11,17 @@ from pickphase import PickPhaseMenu
 class pickphaseView(unittest.TestCase):
 
     def test__save_headers_filterParams(self):
-    	ppm = PickPhaseMenu(gsac, opts, self.axs)
+    	sys.argv[1:] = ['20120124.00520523.bhz.pkl']
+    	gsac, opts = getDataOpts()
+    	axs = getAxes(opts)
+    	setattr(opts,'labelqual',True)
+    	ppm = PickPhaseMenu(gsac, opts, axs)
 
+    	# click the save button
+    	click_save_event = matplotlib.backend_bases.MouseEvent('button_press_event', ppm.axpp.figure.canvas, 63, 481)
+    	ppm.save(click_save_event)
+
+    	#ppm.save_headers_filterParams(self, event):
 
 # ############################################################################### #
 #                                     VIEWS                                       #
