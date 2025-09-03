b1dc743ed66c3d25a20e173bbd673963885c3020
Simon Lloyd <smlloyd@users.noreply.github.com>
2025-09-03 13:49:42 +0100

    feat: add time window picker

--- aimbat/lib/processing.py
+++ aimbat/lib/processing.py
@@ -1,12 +1,28 @@
 """Processing of data for AIMBAT."""
 
 from __future__ import annotations
-from aimbat.lib.iccs import create_iccs_instance
-from pysmo.tools.iccs import stack_pick as _stack_pick
+from aimbat.lib.defaults import get_default
+from aimbat.lib.event import get_active_event
+from aimbat.lib.typing import ProjectDefault
+from pysmo.tools.iccs import (
+    ICCS,
+    stack_pick as _stack_pick,
+    stack_tw_pick as _stack_tw_pick,
+)
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from sqlmodel import Session
+
+
+def create_iccs_instance(session: Session) -> ICCS:
+    active_event = get_active_event(session)
+    return ICCS(
+        seismograms=active_event.seismograms,
+        window_pre=active_event.parameters.window_pre,
+        window_post=active_event.parameters.window_post,
+        plot_padding=get_default(session, ProjectDefault.TIME_WINDOW_PADDING),
+    )
 
 
 def run_iccs(session: Session) -> None:
@@ -15,7 +31,16 @@ def run_iccs(session: Session) -> None:
     session.commit()
 
 
-def stack_pick(session: Session) -> None:
+def stack_pick(session: Session, padded: bool) -> None:
     iccs = create_iccs_instance(session)
-    _stack_pick(iccs)
+    _stack_pick(iccs, padded)
+    session.commit()
+
+
+def stack_tw_pick(session: Session, padded: bool) -> None:
+    iccs = create_iccs_instance(session)
+    _stack_tw_pick(iccs, padded)
+    active_event = get_active_event(session)
+    active_event.parameters.window_pre = iccs.window_pre
+    active_event.parameters.window_post = iccs.window_post
     session.commit()
